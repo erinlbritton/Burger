cf49b3cd785128b20942d9e4745125c58e122014
Erin <erinlbritton@gmail.com>
2018-05-03 20:34:41 -0400

    Initial commit

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+
+-- CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES  ("Luger Burger"),
+        ("Le Pigeon Burger"),
+        ("Double Animal Style"),
+        ("The Original Burger"),
+        ("Whiskey King Burger")
+;
+
+SELECT *
+FROM burgers
+
+-- From http://coolmaterial.com/feature/burger-week-the-burger-bucket-list-25-burgers-you-need-to-try/
